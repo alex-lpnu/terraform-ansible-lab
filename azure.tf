750431c3e620a75c5b5542473a09c1289fee7123
Oleksandr <keysindie@gmail.com>
2021-12-04 17:16:05 +0200

    feat: Reduce ansible targets

--- azure.tf
+++ azure.tf
@@ -104,7 +104,7 @@ resource "tls_private_key" "jupyterssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-output "tls_private_key" {
+output "jupyter_ssh_private_key" {
   value     = tls_private_key.jupyterssh.private_key_pem
   sensitive = true
 }
